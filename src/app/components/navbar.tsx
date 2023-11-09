2beaea27cd76d294361fe327f3d6fb6d1dcd76bb
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-09 18:28:41 +0530

    style changes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -11,14 +11,14 @@ export default function Navbar() {
 
     return(
         <>
-            <div className="w-full  max-h-20  z-20 sticky top-0">
+            <div className="w-full bg-[#292929] max-h-20  z-20 sticky top-0">
                 <Header />
                 <div className=" flex flex-row items-center justify-between mx-auto px-4 lg:px-8">
                     <Link href="/" className="flex items-center ml-0 pt-2 my-2">
                         <div className="">
                             <div className="">
                                 {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
-                                <Image src={logo} alt="logo" className="w-[144px] lg:w-[160px] lg:px-3"/>
+                                <Image src={logo} alt="logo" className="w-[144px] lg:w-40 lg:px-3"/>
                             </div>
                         </div>
                     </Link>
