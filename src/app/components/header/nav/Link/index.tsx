2478a64cfd634756c6536edfac94fcdeb5a0ca09
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-09 18:06:45 +0530

    bug fix v11

--- src/app/components/header/nav/Link/index.tsx
+++ src/app/components/header/nav/Link/index.tsx
@@ -4,16 +4,16 @@ import { motion } from 'framer-motion';
 import { slide, scale } from '../../anim';
 
 
-export default function Index({data, isActive, setSelectedIndicator}) {
+export default function Index({data, isActive, setSelectedIndicator} : {data: never, isActive: boolean, setSelectedIndicator: boolean}) {
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { title, href, index} = data;
 
     return (
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
-        <motion.div className={styles.link} onMouseEnter={() => {setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        <motion.div className={styles.link} onMouseEnter={() => { // @ts-ignore
+            setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
             <motion.div variants={scale} animate={isActive ? "open" : "closed"} className={styles.indicator}></motion.div>
-            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
             <Link href={href}>{title}</Link>
         </motion.div>
     )
