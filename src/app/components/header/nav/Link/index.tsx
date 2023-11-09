4dacd5fbc2b7b0f84d4a333064d82f99874c6950
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-09 17:48:46 +0530

    bug fix v5

--- src/app/components/header/nav/Link/index.tsx
+++ src/app/components/header/nav/Link/index.tsx
@@ -9,9 +9,10 @@ export default function Index({data, isActive, setSelectedIndicator}) {
     const { title, href, index} = data;
 
     return (
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
         <motion.div className={styles.link} onMouseEnter={() => {setSelectedIndicator(href)}} custom={index} variants={slide} initial="initial" animate="enter" exit="exit">
             <motion.div variants={scale} animate={isActive ? "open" : "closed"} className={styles.indicator}></motion.div>
+            {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
             <Link href={href}>{title}</Link>
         </motion.div>
     )
