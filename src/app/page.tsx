55a80e3ce655fd3dcb86fb8da89b054b0f65605e
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-10 01:17:32 +0530

    gallery added to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import Hero from "~/app/components/hero";
-import Faq from "~/app/components/faq";
+import Slider from "~/app/components/imagegallery/image";
+import React from "react";
 
 export default function Home() {
     return (
         <>
             <div className="bg-black">
                 <Hero />
-                <div className="max-w-7xl mx-auto px-4 lg:px-8">
-
+                <div className="">
+                    <div className="w-full py-12 bg-fixed">
+                        <div className=" mx-auto px-4 lg:px-8">
+                            <p className="text-5xl text-center font-bold text-ieeeyellow">Gallery</p>
+                            <hr className="mt-1 border-ieeegray opacity-20"/>
+                        </div>
+                    </div>
+                    <Slider />
+                    <hr className="mt-1 border-ieeegray opacity-20"/>
                 </div>
             </div>
         </>
