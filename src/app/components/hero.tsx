120b799d5a625e9544597d2e477c62dfad518043
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-10 01:46:45 +0530

    minor changes

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     return(
         <>
-            <div className="h-screen w-full">
-                <div className="max-w-7xl mx-auto px-4 lg:px-8">
+            <div className=" ">
+                <div className="mx-auto px-4 lg:px-8">
                     <div className='bg-[#000000]'>
 
                     </div>
