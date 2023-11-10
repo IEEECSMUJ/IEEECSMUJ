3209f570ba0c6f96b74718862eeb3b941097ad0b
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-11 04:22:04 +0530

    spline

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,10 +1,12 @@
+import Spline from '@splinetool/react-spline';
+
 export default function Hero() {
     return(
         <>
             <div className=" ">
                 <div className="mx-auto px-4 lg:px-8">
                     <div className='bg-[#000000]'>
-
+                        <Spline scene="https://prod.spline.design/RY-UMAYj1WqRQbIz/scene.splinecode" />
                     </div>
                 </div>
             </div>
