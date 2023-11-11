a6c9f122c627b06fe0f6db2552e1e3570bbc3955
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-11 11:08:50 +0530

    minor changes

--- src/app/components/team_card.tsx
+++ src/app/components/team_card.tsx
@@ -9,7 +9,7 @@ export function ECTeamCard() {
     return(
         <>
             <div>
-                <div className='mx-auto w-64 border p-2 bg-ieeeorange bg-opacity-10 border-ieeeorange rounded-xl'>
+                <div className='mx-auto w-64 bg-ieeeorange bg-opacity-10 border-ieeeorange rounded-xl'>
                     <div className="relative overflow-hidden transition duration-500 transform rounded-lg">
                         <Image src={pujan} alt="Pujan Modha" className="object-cover rounded-lg w-full aspect-[3/4]"/>
                         <div className="absolute inset-0 flex flex-col justify-end px-4 py-2 transition-opacity duration-300 bg-ieeeorange bg-opacity-100 opacity-0 hover:opacity-100">
@@ -60,7 +60,7 @@ export function CCTeamCard() {
     return(
         <>
             <div>
-                <div className='mx-auto w-64 border p-2 bg-ieeeorange bg-opacity-10 border-ieeeorange rounded-full'>
+                <div className='mx-auto w-64 bg-ieeeorange bg-opacity-10 border-ieeeorange rounded-full'>
                     <div className="relative overflow-hidden transition duration-500 transform rounded-full">
                         <Image src={pujan} alt="Pujan Modha" className="object-cover rounded-full w-full aspect-square"/>
                         <div className="absolute inset-0 flex flex-col justify-end p-16 text-center transition-opacity duration-300 bg-ieeeorange bg-opacity-100 opacity-0 hover:opacity-100">
