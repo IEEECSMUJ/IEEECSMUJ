f6747b47abe23b13e27c0a48880f67e4b24be95e
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-11 10:37:55 +0530

    removed why us

--- src/app/components/mission.tsx
+++ src/app/components/mission.tsx
@@ -25,19 +25,6 @@ const Mission = () => {
                     </div>
                 </div>
             </div>
-            <div className='bg-[#000000]'>
-                <div className=" mx-auto px-4 py-4 lg:px-8">
-                    <p className="text-5xl text-center font-bold text-ieeeyellow">Why us?</p>
-                </div>
-            </div>
-            <div className="max-w-7xl mx-auto px-4 lg:px-8 py-4">
-                <div className='md:mx-16 mx-8 text-center '>
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    <p className='my-4'>We're the world's largest technical society, offering exclusive internship opportunities and abundant tech resources online. Hosting the majority of tech events, we provide diverse skill development and networking opportunities. As a member, enjoy free access to a range of events.</p>
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    <p className='my-4'>You can indulge in coding challenges within active groups, benefit from IEEE senior's success stories, and explore research paper publications. Our family culture encourages collaboration, supported by dedicated seniors, allowing you to build valuable industry connections. Join us for a dynamic and supportive tech community.</p>
-                </div>
-            </div>
         </>
     );
 };
