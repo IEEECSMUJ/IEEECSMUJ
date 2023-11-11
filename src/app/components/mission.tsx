f908a19b6d7bffb14d6e9488fe7b22b78ff4d0a7
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-11 10:32:26 +0530

    bug fixes

--- src/app/components/mission.tsx
+++ src/app/components/mission.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Mission = () => {
     return (
         <>
-            <div className='flex absolute overflow-hidden right-0 -mt-52  mr-6 bg-ieeeyellow rounded-full p-2'>
+            <div className='flex absolute overflow-hidden right-0 -mt-32  mr-6 bg-ieeeyellow rounded-full p-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
                 </svg>
