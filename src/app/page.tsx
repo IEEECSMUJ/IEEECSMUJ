2b23a2cf30da8d6d0ac84a4d1b29238e825cfa40
Arunabha-NOTE <arunabham2017@gmail.com>
2023-11-17 08:39:07 +0530

    Revert "minor fix to scroll element"
    This reverts commit 601625ffa9e73953d7d09c6e9cdf9b280b8b1fcd.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
                 shallowRouting
             />
             <div className='h-screen w-screen top-0 overflow-hidden'><ParticlesContainer /></div>
-            <p className='flex absolute overflow-hidden right-0 -mt-64 text-lg mr-6 text-ieeeyellow -rotate-90'>Scroll</p>
-            <div className='flex absolute overflow-hidden right-0 -mt-48  mr-6 motion-safe:animate-bounce bg-ieeeyellow rounded-full p-2'>
+            <p className='flex absolute overflow-hidden right-0 -mt-48 lg:-mt-64 text-lg mr-6 text-ieeeyellow -rotate-90'>Scroll</p>
+            <div className='flex absolute overflow-hidden right-0 -mt-32 lg:-mt-48  mr-6 motion-safe:animate-bounce bg-ieeeyellow rounded-full p-2'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
                 </svg>
