81eeb0455e588090af3affddc74aaefb4ffd37fe
Arunabha-NOTE <arunabham2017@gmail.com>
2023-12-22 20:54:10 +0530

    added new faq data

--- src/app/data/AccordianData.js
+++ src/app/data/AccordianData.js
@@ -1,6 +1,6 @@
 const AccordionData = [
     {
-        accordionQuestion: 'Who are we?',
+        accordionQuestion: 'Who are we and what is our mission?',
         accordionAnswer: 'We are a vibrant community of tech enthusiasts united by a common goal – to foster a dynamic coding environment through an exciting array of tech and semi-tech events. Our mission is to inspire innovation, collaboration, and continuous learning!'
     },
     {
@@ -11,6 +11,21 @@ const AccordionData = [
         accordionQuestion: 'Are there any prerequisites for joining the club?',
         accordionAnswer: 'Absolutely! All you need is a genuine passion for technology and a determination to learn and grow together. We thrive on encouraging individuals and fostering a community of like-minded enthusiasts.'
     },
+    {
+        accordionQuestion: 'What benefits do members receive?',
+        accordionAnswer: 'As a member, you\'ll experience personal and professional growth. Your soft skills will flourish, your personality will expand, and you\'ll explore new horizons through a diverse range of free events tailored for our members. Membership isn\'t just about learning to code; it\'s about building a well-rounded skill set that extends beyond the digital workspace.'
+    },
+    {
+        accordionQuestion: 'What tech-related workshops or events have we conducted for our members?',
+        accordionAnswer: 'Our repertoire of tech-related workshops and events is extensive. To delve into the details, head over to our dedicated events section',
+    },
+    {
+        accordionQuestion: 'Why us? ',
+        accordionAnswer: 'We\'re the world\'s largest technical society, offering exclusive internship opportunities and abundant tech resources online. Hosting the majority of tech events, we provide diverse skill development and networking opportunities. As a member, enjoy free access to a range of events.\n' +
+            '\n' +
+            'You can indulge in coding challenges within active groups, benefit from IEEE seniors\' success stories, and explore research paper publications. Our family culture encourages collaboration, supported by dedicated seniors, allowing you to build valuable industry connections. Join us for a dynamic and supportive tech community.'
+    }
+
 ];
 
 export default AccordionData;
