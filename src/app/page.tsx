9715149e33342ce34bc31c7ab7d804d48ac2d568
Arunabha-NOTE <arunabham2017@gmail.com>
2024-01-13 21:00:35 +0530

    new hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,65 +6,72 @@ import Mission from "~/app/components/mission";
 import Faq from "./components/faq"
 import useLenis from "~/app/hooks/useLenis";
 import ParticlesContainer from "~/app/components/ParticlesContainer";
-import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import {AppProgressBar as ProgressBar} from 'next-nprogress-bar';
+import ReactPlayer from 'react-player'
 
 
 export default function Home() {
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 5000);
-    }, []);
+    const [playing, setPlaying] = useState(true);
+    // useEffect(() => {
+    //     setLoading(true);
+    //     setTimeout(() => {
+    //         setLoading(false);
+    //     }, 5000);
+    // }, []);
     useLenis();
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+    const [navBarH, setNH] = useState(0)
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
+        setNH((document.querySelector("#navbar"))?.clientHeight)
+    }, []);
+    const isMobile = windowSize.width <= 720 && windowSize.height <= 1280
     return (
-       <div className="">
-           {loading ? <div className="flex h-screen bg-black items-start">
-                   <video
-                       className="videolc h-screen md:h-auto justify-center md:justify-around items-center md:items-start flex -mt-36 md:-mt-40 md:scale-75 scale-100"
-                       id="bg-video"
-                       autoPlay={true}
-                       muted={true}
-                       loop={false}
-                       playsInline={true}
-                   >
-                       <source
-                           src="https://prismic-io.s3.amazonaws.com/ieeemuj/87bcd4f4-9255-43cf-be90-4a0bb6cd1e19_cs-microchip.mp4"
-                           // src="https://video-previews.elements.envatousercontent.com/h264-video-previews/8bc2eb2a-6288-46a0-9873-b1ffeefc00dd/37359639.mp4"
-                           type="video/mp4"
-                       />
-                   </video>
-               </div> :
-           <div>
-               <ProgressBar
-                   height="4px"
-                   color="#FFA300"
-                   options={{ showSpinner: false }}
-                   shallowRouting
-               />
-               <div className='h-screen w-screen top-0 overflow-hidden'><ParticlesContainer /></div>
-               <p className='flex absolute overflow-hidden right-0 -mt-64 lg:-mt-64 text-lg mr-6 text-ieeeyellow -rotate-90'>Scroll</p>
-               <div className='flex absolute overflow-hidden right-0 -mt-48 lg:-mt-48  mr-6 motion-safe:animate-bounce bg-ieeeyellow rounded-full p-2'>
-                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                       <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
-                   </svg>
-               </div>
-               <div id='scroll' className="">
-                   <div className="bg-black">
-                       <Mission />
-                       <div >
-                           <div className="w-full py-12 bg-fixed">
-                               <div className=" mx-auto px-4 lg:px-8" data-aos="fade-up">
-                                   <p className="text-5xl text-center font-bold text-ieeeyellow">Events Gallery</p>
-                               </div>
-                           </div>
-                           <Slider />
-                       </div>
-                       <Faq />
-                   </div>
-               </div>
-           </div>}
-       </div>
+        <div className="">
+            <div>
+                <ProgressBar
+                    height="4px"
+                    color="#FFA300"
+                    options={{showSpinner: false}}
+                    shallowRouting
+                />
+                <div className='h-screen w-screen overflow-hidden relative'>
+                    <ReactPlayer
+                        url='/cs-microchip.mp4'
+                        style={{margin : "auto"}}
+                        playing={playing} progressInterval={100} width={isMobile ? windowSize.width * 0.8 : windowSize.width}
+                        height={isMobile ? windowSize.height * 0.75: windowSize.height - navBarH} onProgress={(n) => {
+                        if (n.playedSeconds >= 4.8) setPlaying(false)
+                    }}/>
+                </div>
+                <p className='flex absolute overflow-hidden right-0 -mt-64 lg:-mt-64 text-lg mr-6 text-ieeeyellow -rotate-90'>Scroll</p>
+                <div
+                    className='flex absolute overflow-hidden right-0 -mt-48 lg:-mt-48  mr-6 motion-safe:animate-bounce bg-ieeeyellow rounded-full p-2'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                         stroke="currentColor" className="w-8 h-8">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"/>
+                    </svg>
+                </div>
+                <div id='scroll' className="">
+                    <div className="bg-black">
+                        <Mission/>
+                        <div>
+                            <div className="w-full py-12 bg-fixed">
+                                <div className=" mx-auto px-4 lg:px-8" data-aos="fade-up">
+                                    <p className="text-5xl text-center font-bold text-ieeeyellow">Events Gallery</p>
+                                </div>
+                            </div>
+                            <Slider/>
+                        </div>
+                        <Faq/>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
