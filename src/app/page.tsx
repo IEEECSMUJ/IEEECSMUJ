8b9d74d4cea2026642d2b152ee228aa2d80e3fe1
Arunabha-NOTE <arunabham2017@gmail.com>
2024-01-14 13:44:24 +0530

    navbar now disappears

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import useLenis from "~/app/hooks/useLenis";
 import ParticlesContainer from "~/app/components/ParticlesContainer";
 import {AppProgressBar as ProgressBar} from 'next-nprogress-bar';
 import ReactPlayer from 'react-player'
+import Header from './components/header/index'
 
 
 export default function Home() {
@@ -22,6 +23,26 @@ export default function Home() {
         setWindowSize({ height: window.innerHeight, width: window.innerWidth });
     }, []);
     const isMobile = windowSize.width <= 720 && windowSize.height <= 1280
+    useEffect(() => {
+        const navbar = document.querySelector("#navbar") as HTMLElement | null;
+        if (navbar) {
+            if (playing) {
+                navbar.style.opacity = "0";
+            } else {
+                navbar.style.opacity = "1";
+            }
+        }
+    }, [playing]);
+    useEffect(() => {
+        const navbar = document.querySelector("#scrollmark") as HTMLElement | null;
+        if (navbar) {
+            if (playing) {
+                navbar.style.opacity = "0";
+            } else {
+                navbar.style.opacity = "1";
+            }
+        }
+    }, [playing]);
     return (
         <div className="">
             <div>
@@ -49,14 +70,16 @@ export default function Home() {
                         }}
                     />
                 </div>
-                <p className='flex absolute overflow-hidden right-0 -mt-64 lg:-mt-64 text-lg mr-6 text-ieeeyellow -rotate-90'>Scroll</p>
-                <div
-                    className='flex absolute overflow-hidden right-0 -mt-48 lg:-mt-48 mr-6 motion-safe:animate-bounce bg-ieeeyellow rounded-full p-2'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="w-8 h-8">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"/>
-                    </svg>
+                <div className='transition duration-500' id='scrollmark'>
+                    <p className='flex absolute overflow-hidden right-0 -mt-64 lg:-mt-64 text-lg mr-6 text-ieeeyellow -rotate-90'>Scroll</p>
+                    <div
+                        className='flex absolute overflow-hidden right-0 -mt-48 lg:-mt-48 mr-6 motion-safe:animate-bounce bg-ieeeyellow rounded-full p-2'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                             stroke="currentColor" className="w-8 h-8">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                  d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5"/>
+                        </svg>
+                    </div>
                 </div>
                 <div id='scroll' className="">
                     <div className="bg-black">
