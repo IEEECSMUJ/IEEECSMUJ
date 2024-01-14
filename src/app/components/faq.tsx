24076235c61098c6fbae01d78bbe99773bce070d
Arunabha-NOTE <arunabham2017@gmail.com>
2024-01-15 01:42:55 +0530

    minor fix

--- src/app/components/faq.tsx
+++ src/app/components/faq.tsx
@@ -84,7 +84,7 @@ const AccordionItem = ({ header, text }) => {
             {/*        <p className='my-4'>Our mission is to inspire innovation, collaboration, and continuous learning!</p>*/}
             {/*    </div>*/}
             {/*</div>*/}
-            <div className="single-faq max-w-7xl mx-auto mt-4 rounded-lg w-full bg-[#292929] bg-opacity-30 backdrop-blur-0 shadow p-4 sm:p-8 lg:px-6 xl:px-8">
+            <div className="single-faq  max-w-7xl mx-auto mt-4 rounded-lg w-full bg-[#292929] bg-opacity-30 backdrop-blur-0 shadow p-4 sm:p-8 lg:px-6 xl:px-8">
                 <button
                     className={`faq-btn flex w-full text-left`}
                     onClick={() => handleToggle()}
@@ -92,7 +92,7 @@ const AccordionItem = ({ header, text }) => {
                     <div className="w-full">
                         <h4 className="lg:text-lg text-base font-semibold text-ieeeyellow">{header}</h4>
                     </div>
-                    <div className="mr-0 flex h-7 w-full max-w-[40px] items-center justify-center rounded-lg bg-ieeeyellow bg-opacity-100 text-ieeeyellow">
+                    <div className=" mr-0 flex h-7 w-full max-w-[40px] items-center justify-center rounded-lg bg-ieeeyellow bg-opacity-100 text-ieeeyellow">
                         <svg
                             className={`duration-250 fill-[#292929] ease-in-out fill-primary stroke-primary ${
                                 active ? "rotate-180" : ""
