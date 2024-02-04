7f04beafc8e8fe70459c65ee60247a91a282d099
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2024-02-04 23:11:33 +0530

    Update CCTeamData.js

--- src/app/data/CCTeamData.js
+++ src/app/data/CCTeamData.js
@@ -1,27 +1,6 @@
 const CCTeamData = [
-    {
-        name: "Arunabha Mukhopadhyay",
-        imageURL: "https://images.prismic.io/ieeemuj/2b3d4e23-0726-4b4b-9461-b6b3b9f54261_WhatsApp+Image+2023-08-13+at+18.00.32.jpg",
-        position: "Senior Coordinator (Programs)",
-        hasLinkedIn: true,
-        linkedInURL: "https://www.linkedin.com/in/arunabha-mukhopadhyay/",
-        hasGitHub: true,
-        gitHubURL: "https://github.com/Arunabha-NOTE",
-        hasInstagram: true,
-        instagramURL: "https://www.instagram.com/arun_note",
-    },
-    {
-        name: "Rishi Joshi",
-        imageURL: "https://images.prismic.io/ieeemuj/d6afed2e-b0ba-484a-a625-6ce8d905d957_rishipic.webp?auto=compress,format",
-        position: "Senior Coordinator Technical Projects",
-        hasLinkedIn: true,
-        linkedInURL: "https://www.linkedin.com/in/rishi-joshi-924882200",
-        hasGitHub: true,
-        gitHubURL: "https://github.com/tangorishi",
-        hasInstagram: true,
-        instagramURL: "https://instagram.com/wtfrishiiiii?igshid=ZDc4ODBmNjlmNQ==",
-    },
-    {
+    
+      {
         name: "Supriya Srivastava",
         imageURL: "https://images.prismic.io/ieeemuj/d143c974-ff5a-4561-8b56-736f9c43ec10_IMG_3268.JPG",
         position: "Head of Programs",
@@ -55,6 +34,17 @@ const CCTeamData = [
         instagramURL: "https://instagram.com/ayush_guptaa1589",
     },
     {
+        name: "Arunabha Mukhopadhyay",
+        imageURL: "https://images.prismic.io/ieeemuj/2b3d4e23-0726-4b4b-9461-b6b3b9f54261_WhatsApp+Image+2023-08-13+at+18.00.32.jpg",
+        position: "Senior Coordinator (Programs)",
+        hasLinkedIn: true,
+        linkedInURL: "https://www.linkedin.com/in/arunabha-mukhopadhyay/",
+        hasGitHub: true,
+        gitHubURL: "https://github.com/Arunabha-NOTE",
+        hasInstagram: true,
+        instagramURL: "https://www.instagram.com/arun_note",
+    },
+    {
         name: "Parv Aggarwal",
         imageURL: "https://images.prismic.io/ieeemuj/b6d5ebfd-290d-4565-8edc-8915219fbb1a_2023-08-13+18_25_49-Parv_Aggarwal.heic+%E2%80%8E-+Photos.png",
         position: "Senior Coordinator (Programs)",
@@ -75,6 +65,17 @@ const CCTeamData = [
         gitHubURL: "",
         hasInstagram: true,
         instagramURL: "https://instagram.com/saniyawadhwanii",
+    },
+    {
+        name: "Rishi Joshi",
+        imageURL: "https://images.prismic.io/ieeemuj/d6afed2e-b0ba-484a-a625-6ce8d905d957_rishipic.webp?auto=compress,format",
+        position: "Senior Coordinator Technical Projects",
+        hasLinkedIn: true,
+        linkedInURL: "https://www.linkedin.com/in/rishi-joshi-924882200",
+        hasGitHub: true,
+        gitHubURL: "https://github.com/tangorishi",
+        hasInstagram: true,
+        instagramURL: "https://instagram.com/wtfrishiiiii?igshid=ZDc4ODBmNjlmNQ==",
     },
     {
         name: "Harshneet Khanna",
