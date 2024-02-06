ac6a7a0037592df44387a56b613e66c42a5f114f
Arunabha-NOTE <arunabham2017@gmail.com>
2024-02-06 11:54:56 +0530

    bug fixes

--- src/app/data/WebTeamData.js
+++ src/app/data/WebTeamData.js
@@ -1,5 +1,16 @@
 const WebTeamData = [
     {
+        name: "Arunabha Mukhopadhyay",
+        imageURL: "https://images.prismic.io/ieeemuj/2b3d4e23-0726-4b4b-9461-b6b3b9f54261_WhatsApp+Image+2023-08-13+at+18.00.32.jpg",
+        position: "Senior Coordinator (Programs)",
+        hasLinkedIn: true,
+        linkedInURL: "https://www.linkedin.com/in/arunabha-mukhopadhyay/",
+        hasGitHub: true,
+        gitHubURL: "https://github.com/Arunabha-NOTE",
+        hasInstagram: true,
+        instagramURL: "https://www.instagram.com/arun_note",
+    },
+    {
         name: "Rishi Joshi",
         imageURL: "https://images.prismic.io/ieeemuj/d6afed2e-b0ba-484a-a625-6ce8d905d957_rishipic.webp?auto=compress,format",
         position: "Senior Coordinator Technical Projects",
@@ -11,16 +22,17 @@ const WebTeamData = [
         instagramURL: "https://instagram.com/wtfrishiiiii?igshid=ZDc4ODBmNjlmNQ==",
     },
     {
-        name: "Arunabha Mukhopadhyay",
-        imageURL: "https://images.prismic.io/ieeemuj/2b3d4e23-0726-4b4b-9461-b6b3b9f54261_WhatsApp+Image+2023-08-13+at+18.00.32.jpg",
-        position: "Senior Coordinator (Programs)",
+        name: "Aryan Jain",
+        imageURL: "https://images.prismic.io/ieeemuj/7901c060-a97e-48fe-9c02-f867c207dcfc_WhatsApp+Image+2023-08-13+at+17.50.19.jpg",
+        position: "Joint Head of Media",
         hasLinkedIn: true,
-        linkedInURL: "https://www.linkedin.com/in/arunabha-mukhopadhyay/",
-        hasGitHub: true,
-        gitHubURL: "https://github.com/Arunabha-NOTE",
+        linkedInURL: "https://www.linkedin.com/in/aryan-jain-2722b6255/",
+        hasGitHub: false,
+        gitHubURL: "",
         hasInstagram: true,
-        instagramURL: "https://www.instagram.com/arun_note",
+        instagramURL: "https://instagram.com/aryan.j/",
     },
+
    
 ]
 
