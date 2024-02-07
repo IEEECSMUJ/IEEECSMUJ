9c6f23cbf9b1f036ce567108b2c44db2d3d17aa4
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2024-02-07 17:57:12 +0530

    Update page.tsx

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,17 +4,21 @@ import React, { useState } from "react";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import Link from "next/link";
 import eventsData from "~/app/data/eventdata";
-import Image from "next/image";
 
 
 export default function Page() {
   const [activeTab, setActiveTab] = useState<string>("COMPLETED");
 
-  // Filter events based on the active tab
+ 
   const displayedEvents =
     activeTab === "COMPLETED"
       ? eventsData.filter((event) => event.id >= 1 && event.id <= 14)
       : eventsData.filter((event) => event.id > 14);
+
+      const handleRegisterClick = () => {
+       
+        window.location.href = "https://forms.gle/H588NBogT49rv2Lx8"; 
+      };
 
   return (
     <>
@@ -54,37 +58,44 @@ export default function Page() {
 
           {/* Event Cards */}
           <div className="grid w-full grid-cols-1 gap-10 pb-8 pt-10 sm:grid-cols-2 md:grid-cols-3">
-            {displayedEvents.map((event) => (
-              <div
-                key={event.id}
-                className="overflow-hidden rounded-lg bg-black shadow-lg"
-              >
-                <Image
-                  src={event.imageUrl}
-                  alt={event.title}
-                  className="h-64 w-full object-cover"
-                  width= {512}
-                  height={512}
-                />
-                <div className="p-4">
-                  <h1 className="text-white-800 text-xl font-semibold">
-                    {event.title}
-                  </h1>
-                  <p className="text-white-600 mt-2 text-sm">
-                    {event.description}
-                  </p>
-                  <Link href={`/events/${event.id}`}>
-                  
-                    <div className="mt-4 block rounded-full bg-[#FFA300] px-4 py-2 font-semibold text-white transition duration-300 hover:bg-yellow-700">
-                      Know More
-                    </div>
-                  </Link>
+        {displayedEvents.map((event) => (
+          <div
+            key={event.id}
+            className="overflow-hidden rounded-lg bg-black shadow-lg w-full"
+          >
+            <img
+              src={event.imageUrl}
+              alt={event.title}
+              className="h-64 w-full object-cover sm:h-80 lg:h-96"
+            />
+            <div className="p-4">
+              <h1 className="text-white-800 text-xl font-semibold">
+                {event.title}
+              </h1>
+              <p className="text-white-600 mt-2 text-sm">
+                {event.description}
+              </p>
+              {event.id === 15 ? (
+                <div
+                  onClick={() => handleRegisterClick()}
+                  className="mt-4 block rounded-full bg-[#FFA300] px-4 py-2 font-semibold text-white transition duration-300 hover:bg-yellow-700 cursor-pointer"
+                >
+                  Register
                 </div>
-              </div>
-            ))}
+              ) : (
+                <Link href={`/events/${event.id}`}>
+                  <div className="mt-4 block rounded-full bg-[#FFA300] px-4 py-2 font-semibold text-white transition duration-300 hover:bg-yellow-700">
+                    Know More
+                  </div>
+                </Link>
+              )}
+            </div>
           </div>
+        ))}
+        </div>
         </div>
       </div>
     </>
   );
 }
+
