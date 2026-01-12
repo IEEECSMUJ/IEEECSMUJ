183bd362dbd245c74599c680b2289e6596963ec5
AwesomeSam9523 <samakshgupta04@gmail.com>
2026-01-12 18:28:16 +0530

    Add FTF page to redirect to WhatsApp meeting link

---
+++ src/app/community/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from 'next/navigation'
+
+export default function FTF() {
+  const meetUrl = "https://chat.whatsapp.com/FiC0TIDJASq2hcRCsuJrIs";
+  redirect(meetUrl);
+
+  return (
+    <div>
+    </div>
+  );
+}
