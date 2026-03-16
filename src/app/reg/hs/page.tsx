e9bc488c5d002fd6998784378aebf0117686ac8a
Tanmoy mandal <tanmaymandal1250@gmail.com>
2026-03-16 14:41:48 +0530

    fixed

---
+++ src/app/reg/hs/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function HackerzStreetRegistration() {
+  redirect("https://unstop.com/p/hackerzstreet-40-manipal-university-mu-jaipur-1657990");
+}
